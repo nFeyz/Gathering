fd03d47913a00efbe11540408da6f972922426ca
海比丶天蓝 <149352394@qq.com>
2019-05-11 17:49:27 +0800

    更新

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = function Gathering(mod) {
-	const command = mod.command || mod.require.command;
+	const Message = require('../tera-message')
+	const MSG = new Message(mod)
 	
 	if (mod.proxyAuthor !== 'caali') {
 		const options = require('./module').options
@@ -19,6 +20,22 @@ module.exports = function Gathering(mod) {
 	let mobid = [],
 		gatherMarker = []
 	
+	function gatheringStatus() {
+		sendStatus(
+			"Gathering: " + (mod.settings.enabled ? MSG.BLU("开启") : MSG.YEL("关闭")),
+			"警告消息: " + (mod.settings.sendToAlert  ? MSG.BLU("启用") : MSG.YEL("禁用")),
+			"通知消息: " + (mod.settings.sendToNotice ? MSG.BLU("启用") : MSG.YEL("禁用")),
+			
+			"植物提示: " + (plantsMarkers ? MSG.BLU("启用") : MSG.YEL("禁用")),
+			"矿石提示: " + (miningMarkers ? MSG.BLU("启用") : MSG.YEL("禁用")),
+			"精气提示: " + (energyMarkers ? MSG.BLU("启用") : MSG.YEL("禁用"))
+		)
+	}
+	
+	function sendStatus(msg) {
+		MSG.chat([...arguments].join('\n\t - '))
+	}
+	
 	mod.command.add("采集", (arg) => {
 		if (!arg) {
 			mod.settings.enabled = !mod.settings.enabled;
@@ -30,16 +47,16 @@ module.exports = function Gathering(mod) {
 					despawnItem(itemId)
 				}
 			}
-			sendMessage("模块 " + (mod.settings.enabled ? BLU("开启") : YEL("关闭")))
+			MSG.chat("Gathering " + (mod.settings.enabled ? MSG.BLU("开启") : MSG.YEL("关闭")))
 		} else {
 			switch (arg) {
 				case "警告":
 					mod.settings.sendToAlert = !mod.settings.sendToAlert
-					sendMessage("警告消息 " + (mod.settings.sendToAlert ? BLU("启用") : YEL("禁用")))
+					MSG.chat("警告消息 " + (mod.settings.sendToAlert ? MSG.BLU("启用") : MSG.YEL("禁用")))
 					break
 				case "通知":
 					mod.settings.sendToNotice = !mod.settings.sendToNotice
-					sendMessage("通知消息 " + (mod.settings.sendToNotice ? BLU("启用") : YEL("禁用")))
+					MSG.chat("通知消息 " + (mod.settings.sendToNotice ? MSG.BLU("启用") : MSG.YEL("禁用")))
 					break
 					
 				case "状态":
@@ -48,19 +65,19 @@ module.exports = function Gathering(mod) {
 				
 				case "植物":
 					plantsMarkers = !plantsMarkers
-					sendMessage("植物提示 " + (plantsMarkers ? BLU("显示") : YEL("隐藏")))
+					MSG.chat("植物提示 " + (plantsMarkers ? MSG.BLU("显示") : MSG.YEL("隐藏")))
 					break
 				case "矿石":
 					miningMarkers = !miningMarkers
-					sendMessage("矿石提示 " + (miningMarkers ? BLU("显示") : YEL("隐藏")))
+					MSG.chat("矿石提示 " + (miningMarkers ? MSG.BLU("显示") : MSG.YEL("隐藏")))
 					break
 				case "精气":
 					energyMarkers = !energyMarkers
-					sendMessage("精气提示 " + (energyMarkers ? BLU("显示") : YEL("隐藏")))
+					MSG.chat("精气提示 " + (energyMarkers ? MSG.BLU("显示") : MSG.YEL("隐藏")))
 					break
 				
 				default :
-					sendMessage(RED("无效的参数!"))
+					MSG.chat("Gathering " + MSG.RED("无效的参数!"))
 					break
 			}
 		}
@@ -73,14 +90,14 @@ module.exports = function Gathering(mod) {
 	mod.hook('S_SPAWN_COLLECTION', 4, (event) => {
 		if (mod.settings.enabled) {
 			if (plantsMarkers && (gatherMarker = mod.settings.plants.find(obj => obj.id === event.id))) {
-				alertMessage( "发现 [" + gatherMarker.name + "] " + gatherMarker.msg)
-				noticeMessage("发现 [" + gatherMarker.name + "] " + gatherMarker.msg)
+				MSG.alert(("发现 [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
+				MSG.raids( "发现 [" + gatherMarker.name + "] " + gatherMarker.msg)
 			} else if (miningMarkers && (gatherMarker = mod.settings.mining.find(obj => obj.id === event.id))) {
-				alertMessage( "发现 [" + gatherMarker.name + "] " + gatherMarker.msg)
-				noticeMessage("发现 [" + gatherMarker.name + "] " + gatherMarker.msg)
+				MSG.alert(("发现 [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
+				MSG.raids( "发现 [" + gatherMarker.name + "] " + gatherMarker.msg)
 			} else if (energyMarkers && (gatherMarker = mod.settings.energy.find(obj => obj.id === event.id))) {
-				alertMessage( "发现 [" + gatherMarker.name + "] " + gatherMarker.msg)
-				noticeMessage("发现 [" + gatherMarker.name + "] " + gatherMarker.msg)
+				MSG.alert(("发现 [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
+				MSG.raids( "发现 [" + gatherMarker.name + "] " + gatherMarker.msg)
 			} else {
 				return true
 			}
@@ -116,59 +133,6 @@ module.exports = function Gathering(mod) {
 		mod.send('S_DESPAWN_DROPITEM', 4, {
 			gameId: gameId*100n
 		})
-	}
-	
-	function alertMessage(msg) {
-		if (mod.settings.sendToAlert) {
-			mod.send('S_DUNGEON_EVENT_MESSAGE', 2, {
-				type: 43,
-				chat: 0,
-				channel: 0,
-				message: msg
-			})
-		}
-	}
-	
-	function noticeMessage(msg) {
-		if (mod.settings.sendToNotice) {
-			mod.send('S_CHAT', 2, {
-				channel: 25,
-				authorName: "TIP",
-				message: msg
-			})
-		}
-	}
-	
-	function gatheringStatus() {
-		sendStatus(
-			`模块 : ${mod.settings.enabled ? BLU('开启') : YEL('关闭')}`,
-			`警告消息 : ${mod.settings.sendToAlert ? BLU('启用') : YEL('禁用')}`,
-			`通知消息 : ${mod.settings.sendToNotice ? BLU('启用') : YEL('禁用')}`,
-			
-			`植物提示 : ${plantsMarkers ? BLU('显示') : YEL('隐藏')}`,
-			`矿石提示 : ${miningMarkers ? BLU('显示') : YEL('隐藏')}`,
-			`精气提示 : ${energyMarkers ? BLU('显示') : YEL('隐藏')}`
-		)
-	}
-	
-	function sendStatus(msg) {
-		command.message([...arguments].join('\n\t - '))
-	}
-	
-	function sendMessage(msg) {
-		command.message(msg)
-	}
-	
-	function BLU(bluetext) {
-		return '<font color="#56B4E9">' + bluetext + '</font>'
-	}
-	
-	function YEL(yellowtext) {
-		return '<font color="#E69F00">' + yellowtext + '</font>'
-	}
-	
-	function RED(redtext) {
-		return '<font color="#FF0000">' + redtext + '</font>'
 	}
 	
 }
